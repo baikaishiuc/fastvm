f7bfa16bcd973b0d54982e3b461c2dc6a6fab8ba
baikaishiuc <baikaishiuc@gmail.com>
2020-04-20 08:08:11 +0800

    UPDATE:sync

--- src/fastvm/arm_emu.c
+++ src/fastvm/arm_emu.c
@@ -65,7 +65,7 @@ struct arm_inst_desc {
     {"0000    o1    i5 l3 l3",        {thumb_inst_lsl, thumb_inst_lsl}},
     {"0001    0    i5 l3 l3",        {thumb_inst_asr}},
     {"1011    o1    i5 l3 l3",        {thumb_inst_push, thumb_inst_pop}},
-    {"1110 1001 0010 1101 0M0 r13", {t2_inst_push}}
+    {"1110 1001 0010 1101 0 m1 0 l13", {t2_inst_push}}
 };
 
 static int init_inst_map = 0;
@@ -73,12 +73,10 @@ static int init_inst_map = 0;
 struct inst_node {
     int id;
     struct inst_node *parent;
-    struct inst_node *left;
-    struct inst_node *right;
-    struct inst_node *rep;
+	struct inst_node *childs[3];
 
     arm_inst_func func;
-    struct inst_node *set[3];
+	struct dynarray set;
     char *exp;
 };
 
@@ -105,7 +103,7 @@ struct inst_node *inst_node_new(struct inst_tree *tree, struct inst_node *parent
         vm_error("inst_node_new() failed calloc");
 
     node->parent = parent;
-    node->id = tree->counts++;
+	node->id = ++tree->counts;
     dynarray_add(&tree->arr, node);
 
     return node;
@@ -130,16 +128,53 @@ int inst_node_height(struct inst_node *node)
 
 void    inst_node_delete(struct inst_node *root)
 {
+	int i;
     if (!root)
         return;
 
-    if (root->left)        inst_node_delete(root->left);
-    if (root->right)    inst_node_delete(root->right);
-    if (root->rep)        inst_node_delete(root->rep);
+	for (i = 0; i < count_of_array(root->childs); i++) {
+		inst_node_delete(root->childs[i]);
+	}
 
     if (root->exp)    free(root->exp);
 
     free(root);
+}
+
+void inst_node__dump_dot(FILE *fp, struct inst_node *root)
+{
+	int i;
+
+	for (i = 0; i < count_of_array(root->childs); i++) {
+		if (!root->childs[i])
+			continue;
+
+		fprintf(fp, "%d -> %d [label = \"%d-%d\"]\n", root->id, root->childs[i]->id, i, inst_node_height(root->childs[i]));
+		inst_node__dump_dot(fp, root->childs[i]);
+	}
+
+	if (root->func) {
+		fprintf(fp, "%d [shape=Msquare];\n", root->id);
+	}
+}
+
+void inst_node_dump_dot(const char *filename, struct inst_node *root)
+{
+	char buf[128];
+	FILE *fp = fopen(filename, "w");
+
+	fprintf(fp, "digraph G {\n");
+
+	inst_node__dump_dot(fp, root);
+
+	fprintf(fp, "%d [shape=Mdiamond];\n", root->id);
+
+	fprintf(fp, "}\n");
+
+	fclose(fp);
+
+	sprintf(buf, "dot -Tpng %s -o %s.png", filename, filename);
+	system(buf);
 }
 
 struct arm_inst_engine *inst_engine_create()
@@ -156,48 +191,45 @@ struct arm_inst_engine *inst_engine_create()
 int arm_insteng_add_exp(struct arm_inst_engine *en, const char *exp, arm_inst_func func)
 {
     struct inst_node *root = &en->enfa.root;
-    int j, len = strlen(exp), rep, height;
+    int j, len = strlen(exp), rep, height, idx;
     const char *s = exp;
 
     for (; *s; s++) {
         if (isblank(*s))    continue;
         switch (*s) {
         case '0':
-            if (!root->left) {
-                root->left = inst_node_new(&en->enfa, root);
-            }
+		case '1':
+			idx = *s - '0';
+			if (!root->childs[idx]) {
+                root->childs[idx] = inst_node_new(&en->enfa, root);
+			}
 
-            root = root->left;
+			root = root->childs[idx];
             break;
 
-        case '1':
-            if (!root->right) {
-                root->right = inst_node_new(&en->enfa, root);
-            }
-
-            root = root->right;
-            break;
+            /* immediate */
+        case 'i':
 
             /* register */
-        case 'i':
         case 'l':
+
+            /* more register，一般是对寄存器列表的补充 */
+        case 'm':
             rep = atoi(&s[1]);
             if (!rep) 
                 goto fail_label;
 
             for (j = 0; j < rep; j++) {
-                root->rep = inst_node_new(&en->enfa, root);
-                root = root->rep;
+                root->childs[2] = inst_node_new(&en->enfa, root);
+                root = root->childs[2];
             }
-
-            root = root->rep;
 
             while (s[1] && isdigit(s[1])) s++;
             break;
 
     fail_label:
         default:
-            vm_error("inst expression [%s], position[%d]\n", exp, (exp - s));
+            vm_error("inst expression [%s], position[%d:%s]\n", exp, (s - exp), s);
             break;
         }
     }
@@ -221,46 +253,52 @@ static int arm_insteng_gen_dfa(struct arm_inst_engine *eng)
     struct inst_node *nfa_root = &eng->enfa.root;
 
     struct inst_node *dfa_root = &eng->dfa.root;
-    struct inst_node *node_stack[512], *droot, *nroot, *dnode1;
-    int stack_top = -1, i;
+    struct inst_node *node_stack[512], *droot, *nroot, *dnode1, *nnode;
+    int stack_top = -1, i, j;
 
-#define istack_push(a)        (node_stack[stack_top++] = a)
-#define istack_pop()        node_stack[stack_top--]        
-#define istack_is_empty()    (stack_top == -1)
+#define istack_push(a)            (node_stack[++stack_top] = a)
+#define istack_pop()            node_stack[stack_top--]        
+#define istack_is_empty()        (stack_top == -1)
 
-    dfa_root->set[0] = nfa_root;
+	dynarray_add(&dfa_root->set, nfa_root);
     istack_push(dfa_root);
 
     while (!istack_is_empty()) {
         droot = istack_pop();
 
-        for (i = 0; i < 3; i++) {
-            nroot = droot->set[i];
-            if (!nroot)
-                continue;
+		for (i = 0; i < 2; i++) {
+			dnode1 = NULL;
+			for (j = 0; j < droot->set.len; j++) {
+				if (!(nroot = (struct inst_node *)droot->set.ptab[j]))
+					continue;
 
-            if (nroot->left || nroot->rep) {
-                dnode1 = droot->left = inst_node_new(&eng->dfa, droot);
+				if (nroot->childs[i] || nroot->childs[2]) {
+					if (!droot->childs[i]) {
+						droot->childs[i] = inst_node_new(&eng->dfa, droot);
+						istack_push(droot->childs[i]);
+					}
 
-                droot->left->set[0] = nroot->left;
-                droot->left->set[1] = nroot->rep;
-            }
-            else if (nroot->right || nroot->rep) {
-                dnode1 = droot->right = inst_node_new(&eng->dfa, droot);
+					dnode1 = droot->childs[i];
 
-                droot->right->set[0] = nroot->right;
-                droot->right->set[1] = nroot->rep;
-            }
+					if (nroot->childs[i])	dynarray_add(&dnode1->set, nroot->childs[i]);
+					if (nroot->childs[2])	dynarray_add(&dnode1->set, nroot->childs[2]);
+				}
+			}
 
-            if (dnode1->set[0]->func && dnode1->set[1]->func) {
-                vm_error("Arm Inst expression conflict [%s] [%s]\n", dnode1->set[0]->exp, dnode1->set[1]->exp);
-            }
-            else if (dnode1->set[0]->func || dnode1->set[1]->func) {
-                inst_node_copy(dnode1, dnode1->set[0] ? dnode1->set[0]:dnode1->set[1]);
-            }
+			/* 检查生成的DFA节点中包含的NFA节点，是否有多个终结态，有的话报错，没有的话把NFA的
+			状态拷贝到DFA中去 */
+			if (dnode1) {
+				for (j = 0; j < dnode1->set.len; j++) {
+					if (!(nnode = (struct inst_node *)dnode1->set.ptab[j]) || !nnode->func)	continue;
 
-            istack_push(dnode1);
-        }
+					if (dnode1->func) {
+						vm_error("conflict end state\n");
+					}
+
+					inst_node_copy(dnode1, nnode);
+				}
+			}
+		}
     }
 
     return 0;
@@ -268,14 +306,14 @@ static int arm_insteng_gen_dfa(struct arm_inst_engine *eng)
 
 static void arm_insteng_trav_tree(struct arm_inst_engine *eng, struct inst_node *root)
 {
-    if (root->left) {
-        eng->trans2d[root->id] = root->left->id;
-        arm_insteng_trav_tree(eng, root->left);
-    }
-    if (root->right) {
-        eng->trans2d[eng->width + root->id] = root->right->id;
-        arm_insteng_trav_tree(eng, root->right);
-    }
+	int i;
+
+	for (i = 0; i < 2; i++) {
+		if (!root->childs[i]) continue;
+
+		eng->trans2d[eng->width * i + root->id] = root->childs[i]->id;
+        arm_insteng_trav_tree(eng, root->childs[i]);
+	}
 }
 
 static int arm_insteng_gen_trans2d(struct arm_inst_engine *eng)
@@ -286,6 +324,8 @@ static int arm_insteng_gen_trans2d(struct arm_inst_engine *eng)
     eng->trans2d = calloc(1, eng->width * eng->height * sizeof (eng->trans2d[0]));
     if (!eng->trans2d)
         vm_error("trans2d calloc failure");
+
+	arm_insteng_trav_tree(eng, &eng->dfa.root);
 
     return 0;
 }
@@ -315,6 +355,8 @@ static int arm_insteng_init()
                 if (!bits)
                     vm_error("express error, unknown o token, %s\n", exp);
 
+                while (isdigit(exp[++j]));
+
                 int pow = bits ^ 2;
                 while (pow--) {
                     for (m = 0; m < bits; m++) {
@@ -336,7 +378,12 @@ static int arm_insteng_init()
             arm_insteng_add_exp(g_eng, buf, desclist[i].funclist[0]);
     }
 
+
     arm_insteng_gen_dfa(g_eng);
+
+	inst_node_dump_dot("enfa.dot", &g_eng->enfa.root);
+	inst_node_dump_dot("dfa.dot", &g_eng->dfa.root);
+
     arm_insteng_gen_trans2d(g_eng);
 
     init_inst_map = 1;
@@ -348,7 +395,7 @@ static int arm_insteng_uninit()
 {
 }
 
-static int arm_insteng_decode(struct arm_inst_engine *eng, uint8_t *code, int len)
+static int arm_insteng_decode(uint8_t *code, int len)
 {
     struct inst_node *node;
     int i, j, from, to, bit;
@@ -362,12 +409,12 @@ static int arm_insteng_decode(struct arm_inst_engine *eng, uint8_t *code, int le
         uint16_t inst = ((uint16_t *)code)[i];
         for (j = 0; j < 16; j++) {
             bit = !!(inst & (1 << (15 - i)));
-            to = eng->trans2d[eng->width * bit + from];
+            to = g_eng->trans2d[g_eng->width * bit + from];
 
             if (!to) 
                 vm_error("arm_insteng_decode() run on un-support state, code[%02x %02x]\n", code[0], code[1]);
 
-            node = ((struct inst_node *)eng->dfa.arr.ptab[to]);
+            node = ((struct inst_node *)g_eng->dfa.arr.ptab[to]);
             from = node->id;
         }
 
@@ -410,7 +457,7 @@ int        arm_emu_run(struct arm_emu *emu)
     int len = emu->code.len - emu->code.pos;
     int ret;
 
-    ret = arm_insteng_decode(g_eng, emu->code.data, emu->code.len);
+    ret = arm_insteng_decode(emu->code.data, emu->code.len);
     if (ret < 0) {
         return -1;
     }
