1b0a18848397fba71afe07e5fb533c72adf6f323
baikaishiuc <baikaishiuc@gmail.com>
2020-06-06 10:57:10 +0800

    UPDATE:sh.h

---
+++ src/fastvm/sh.h
@@ -0,0 +1,7 @@
+﻿
+#define GEN_SH          "#!/bin/bash\n"  \
+    "for filename in `find . -type f -name \"*.dot\" | xargs`\n"  \
+    "do\n" \
+    "   echo gen $filename png \n" \
+    "   dot -Tpng -o ${filename%%.*}.png $filename\n" \
+    "done\n" 
